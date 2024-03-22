459e84dbc28fde9e1cb7869042ae9413747f9450
Komal305 <komalrani2861@gmail.com>
2024-03-22 14:26:59 +0530

    reverse array
    resverse array in two way

---
+++ src/arraysJava/ReverseArray.java
@@ -0,0 +1,23 @@
+package arraysJava;
+
+public class ReverseArray {
+public static void main(String[] args) {
+	int[] a= {1,2,3,4,5,6};
+	reverse(a, 0, a.length-1);
+	
+}
+public static void reverse(int[] a,int start,int end) {
+	while(start<end) {
+		int temp=a[start];
+		a[start++]=a[end];
+		a[end--]=temp;
+	}
+	
+//	for(int i=start;i<end;i++) {
+//		int t=a[start];
+//		a[start++]=a[end];
+//		a[end--]=t;
+//	}
+	for(int f:a) System.out.print(f+" ");
+}
+}
